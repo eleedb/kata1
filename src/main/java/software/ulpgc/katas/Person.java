32c99c68c02bda6bb6aafa36c1a8e862cbb6110c
Elena <elena.diazbatista113@gmail.com>
2026-09-23 09:19:57 +0100

    Person with name atribute

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,4 +1,15 @@
 package software.ulpgc.katas;
 
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
 }
