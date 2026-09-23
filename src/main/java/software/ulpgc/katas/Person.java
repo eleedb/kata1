677216cb7b5e2c7a3d210b42e4f7387d82e2cd43
Elena <elena.diazbatista113@gmail.com>
2026-09-23 09:21:14 +0100

    Person as record

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -2,14 +2,5 @@ package software.ulpgc.katas;
 
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 
-public class Person {
-    private final String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public String name() {
-        return name;
-    }
+public record Person(String name) {
 }
