732a0a172da30a679128dc9540a4c0dff33c2bc2
Elena <elena.diazbatista113@gmail.com>
2026-09-23 09:23:00 +0100

    Person with birthdate atribute

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,6 +1,7 @@
 package software.ulpgc.katas;
 
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.time.LocalDate;
 
-public record Person(String name) {
+public record Person(String name, LocalDate birthdate) {
 }
