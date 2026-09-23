7fce0d2687bb0dcbe2a008097b36fef1ed1963e8
Elena <elena.diazbatista113@gmail.com>
2026-09-23 09:36:45 +0100

    Main updated

--- src/main/java/software/ulpgc/katas/Main.java
+++ src/main/java/software/ulpgc/katas/Main.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class Main {
     static void main() {
-        Person person = new Person("lucia", LocalDate.of(2005, 3, 2));
+        Person person = new Person("Lucia", LocalDate.of(2005, 3, 2));
         System.out.println(person.age());
     }
 }
