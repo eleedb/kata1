57eaa528d06eb594379b2092de8da544a979e00f
Elena <elena.diazbatista113@gmail.com>
2026-09-23 09:31:08 +0100

    Person with age atribute

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -4,4 +4,13 @@ import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import java.time.LocalDate;
 
 public record Person(String name, LocalDate birthdate) {
+
+    public int age(){
+        return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+
+    private static final double DAYS_PER_YEAR = 365.25;
+    private int toYears(long days) {
+        return (int) (days/ DAYS_PER_YEAR);
+    }
 }
